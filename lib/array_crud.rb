96d8d750e86af66f02e2cd6c00745c1d67deb00d
Louis Leffler <louisleffler@gmail.com>
2020-11-24 18:42:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  ["John", "Paul", "George"]
+  add_element_to_start_of_array.push("Ringo")
 end 
 
 def add_element_to_start_of_array(array, element)
